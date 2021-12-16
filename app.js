4dacf51c407232f9e525f01d8b0890b64d31d28e
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-16 16:46:36 +0100

    first step to eval

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// let depenses
+let loyer = document.getElementById('loyer');
+let facture = document.getElementById('facture');
+let internet = document.getElementById('internet');
+let voiture = document.getElementById('voiture');
+let course = document.getElementById('course');
+let transport = document.getElementById('transport');
+let activite = document.getElementById('activite');
+let sortie = document.getElementById('sortie');
+let autre = document.getElementById('autre');
+
+//let recette
+let salaire = document.getElementById('salaire');
+let aide = document.getElementById('aide');
+let rente = document.getElementById('rente');
+let autres = document.getElementById('autres');
+
+//let epargne
+let epargne = document.getElementById('epargne');
+
+//let result
+let resultDepenses = document.getElementById('result-depenses');
+let resultRecette = document.getElementById('result-recette');
+let resulEpargne = document.getElementById('result-epargne');
+let result = document.getElementById('result');
+
+
+
+
