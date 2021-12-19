7cb2f50fe28c20c0d332b5b87238c5c351769163
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-19 10:50:47 +0100

    finish eval buget

--- app.js
+++ app.js
@@ -22,9 +22,54 @@ let epargne = document.getElementById('epargne');
 //let result
 let resultDepenses = document.getElementById('result-depenses');
 let resultRecette = document.getElementById('result-recette');
-let resulEpargne = document.getElementById('result-epargne');
+let resultEpargne = document.getElementById('result-epargne');
 let result = document.getElementById('result');
 
+//let button
+let buttonDepense = document.getElementById('buttonDepense');
+let buttonRecette = document.getElementById('buttonRecette');
+let buttonEpargne = document.getElementById('buttonEpargne');
+let buttonTotal = document.getElementById('buttonTotal');
 
 
+buttonDepense.addEventListener('click' , function (){
+    resultDepenses.innerHTML = parseInt(loyer.value)
+        + parseInt(facture.value)
+        + parseInt(internet.value)
+        + parseInt(voiture.value)
+        + parseInt(course.value)
+        + parseInt(transport.value)
+        + parseInt(activite.value)
+        + parseInt(sortie.value)
+        + parseInt(autre.value);
+})
+
+buttonRecette.addEventListener('click' , function (){
+    resultRecette.innerHTML = parseInt(salaire.value)
+        + parseInt(aide.value)
+        + parseInt(rente.value)
+        + parseInt(autres.value);
+})
+
+buttonEpargne.addEventListener('click' , function (){
+    resultEpargne.innerHTML = parseInt(epargne.value);
+})
+
+buttonTotal.addEventListener('click' , function (){
+    result.innerHTML = parseInt(epargne.value)
+        + parseInt(aide.value)
+        + parseInt(rente.value)
+        + parseInt(autres.value)
+
+        - parseInt(loyer.value)
+        - parseInt(facture.value)
+        - parseInt(internet.value)
+        - parseInt(voiture.value)
+        - parseInt(course.value)
+        - parseInt(transport.value)
+        - parseInt(activite.value)
+        - parseInt(sortie.value)
+        - parseInt(autre.value);
+
+})
 
